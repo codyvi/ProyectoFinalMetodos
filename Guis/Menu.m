b46a5638d42a564bca2e4e299c7ad0dc8dd7980b
David Cantú <32000336+codyvi@users.noreply.github.com>
2019-05-04 22:29:19 -0500

    Added Menu and Problema 2 working

---
+++ Guis/Menu.m
@@ -0,0 +1,109 @@
+function varargout = Menu(varargin)
+% MENU MATLAB code for Menu.fig
+%      MENU, by itself, creates a new MENU or raises the existing
+%      singleton*.
+%
+%      H = MENU returns the handle to a new MENU or the handle to
+%      the existing singleton*.
+%
+%      MENU('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MENU.M with the given input arguments.
+%
+%      MENU('Property','Value',...) creates a new MENU or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Menu_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Menu_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Menu
+
+% Last Modified by GUIDE v2.5 04-May-2019 22:22:17
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Menu_OpeningFcn, ...
+                   'gui_OutputFcn',  @Menu_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Menu is made visible.
+function Menu_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Menu (see VARARGIN)
+
+% Choose default command line output for Menu
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Menu wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Menu_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in Problema1but.
+function Problema1but_Callback(hObject, eventdata, handles)
+%Lit solo pon el nombre del archivo fig sin la extensión y ya 
+%queda bro
+Problema1
+
+
+% --- Executes on button press in Problema2but.
+function Problema2but_Callback(hObject, eventdata, handles)
+% hObject    handle to Problema2but (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+Problema2
+
+
+% --- Executes on button press in Problema3butt.
+function Problema3butt_Callback(hObject, eventdata, handles)
+% hObject    handle to Problema3butt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in Problema4butt.
+function Problema4butt_Callback(hObject, eventdata, handles)
+% hObject    handle to Problema4butt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in Problema5butt.
+function Problema5butt_Callback(hObject, eventdata, handles)
+% hObject    handle to Problema5butt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
